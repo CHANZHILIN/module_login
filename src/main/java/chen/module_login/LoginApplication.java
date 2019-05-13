3062b1e7d0cbc5974b5520136286fb7d68afd079
chanzhilin <1181785848@qq.com>
2019-05-13 17:26:55 +0800

    组件化application

---
+++ src/main/java/chen/module_login/LoginApplication.java
@@ -0,0 +1,23 @@
+package chen.module_login;
+
+import android.app.Application;
+
+import chen.baselib.BaseApplication;
+import chen.baselib.IComponentApplication;
+
+/**
+ * Introduce :
+ * Created by CHEN_ on 2019/5/9.
+ * PACKAGE_NAME : chen
+ **/
+public class LoginApplication implements IComponentApplication {
+    @Override
+    public void onCreate(BaseApplication application) {
+
+    }
+
+    @Override
+    public Application getAppliaction() {
+        return null;
+    }
+}
