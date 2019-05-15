679f39ac26f2c4999d7a3da7f5df890a4d246299
chanzhilin <1181785848@qq.com>
2019-05-15 09:59:45 +0800

    ..

--- src/main/java/chen/module_login/SecondActivity.java
+++ src/main/java/chen/module_login/SecondActivity.java
@@ -13,7 +13,6 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_second);
     }
 }
